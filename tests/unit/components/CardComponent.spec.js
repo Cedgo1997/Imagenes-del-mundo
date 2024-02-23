8fe6cb5981fd67b742f9c6d36fd146b3f4b4c1cd
cesarisim0 <cedgo1997@gmail.com>
2024-02-23 14:57:37 -0400

    FIX: Improve Card unit testing with beforeEach

--- tests/unit/components/CardComponent.spec.js
+++ tests/unit/components/CardComponent.spec.js
@@ -1,47 +1,37 @@
 import { shallowMount } from "@vue/test-utils";
 import CardComponent from "@/components/CardComponent.vue";
 
+const seller = {
+    id: 1,
+    name: "Peter Parker",
+    image: {
+        src: {
+            original: "https://example.com/image.jpg",
+        },
+        alt: "Image Alt Text",
+    },
+};
+
+let wrapper;
+
 describe("CardComponent", () => {
-    it("renders correctly", () => {
-        const seller = {
-            id: 1,
-            name: "John Doe",
-            image: {
-                src: {
-                    original: "https://example.com/image.jpg",
-                },
-                alt: "user image",
-            },
-        };
-        const wrapper = shallowMount(CardComponent, {
+    beforeEach(() => {
+        wrapper = shallowMount(CardComponent, {
             propsData: {
                 seller,
             },
         });
+    });
+
+    it("renders correctly", () => {
         expect(wrapper.exists()).toBe(true);
-        expect(wrapper.find(".card__top h3").text()).toBe("John Doe");
+        expect(wrapper.find(".card__top h3").text()).toBe("Peter Parker");
         expect(wrapper.find(".image-container img").attributes("src")).toBe(
             "https://example.com/image.jpg"
         );
     });
 
     it("toggles like when clicked", async () => {
-        const seller = {
-            id: 1,
-            name: "John Doe",
-            image: {
-                src: {
-                    original: "https://example.com/image.jpg",
-                },
-                alt: "user image",
-            },
-        };
-        const wrapper = shallowMount(CardComponent, {
-            propsData: {
-                seller,
-            },
-        });
-
         expect(wrapper.vm.liked).toBe(false);
         await wrapper.find(".heart-icon").trigger("click");
         expect(wrapper.vm.liked).toBe(true);
@@ -50,26 +40,21 @@ describe("CardComponent", () => {
     });
 
     it("updates score when card is liked", async () => {
-        const seller = {
-            id: 1,
-            name: "John Doe",
-            image: {
-                src: {
-                    original: "https://example.com/image.jpg",
-                },
-                alt: "user image",
-            },
-        };
-        const wrapper = shallowMount(CardComponent, {
-            propsData: {
-                seller,
-            },
-        });
-
         expect(wrapper.vm.score).toBe(0);
         await wrapper.find(".heart-icon").trigger("click");
         expect(wrapper.vm.score).toBe(3);
         await wrapper.find(".heart-icon").trigger("click");
         expect(wrapper.vm.score).toBe(0);
+    });
+
+    it("Download link has the right href value", () => {
+        const wrapper = shallowMount(CardComponent, {
+            props: {
+                seller,
+            },
+        });
+        const downloadButton = wrapper.find(".download-button");
+        const expectedLink = seller.image.src.original;
+        expect(downloadButton.attributes("href")).toBe(expectedLink);
     });
 });
