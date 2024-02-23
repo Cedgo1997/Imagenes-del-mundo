7ef63cba131ad90f02f51c4ab4bfe8f9999d9329
cesarisim0 <cedgo1997@gmail.com>
2024-02-22 23:33:11 -0400

    FEAT: Isolating searchbar in its own component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,7 @@
 <template>
   <h1>World Images</h1>
   <!-- Searchbar -->
-  <div class="searchbar">
-    <IconSearch class="btn btn-left" :size="30" />
-    <input class="searchbar__field" type="text" v-model="seearchTerm" @keypress.enter="getImages(seearchTerm)">
-    <IconX class="btn btn-right" :size="30" />
-  </div>
-
+  <SearchbarComponent @queryText="getImages" />
   <!-- Sellers Component -->
   <div class="card-container" v-if="images.length">
     <CardComponent v-for="image in images" :image="image" :key="image.id" />
@@ -15,25 +10,23 @@
 
 <script>
 import CardComponent from './CardComponent'
+import SearchbarComponent from './SearchbarComponent'
 import getImagesByText from '@/helpers/pexels/getPexelsImages';
-import { IconSearch, IconX } from '@tabler/icons-vue'
 
 export default {
   components: {
     CardComponent,
-    IconSearch,
-    IconX
+    SearchbarComponent
   },
   data() {
     return {
-      seearchTerm: '',
       images: []
     }
   },
   methods: {
     async getImages(query) {
       try {
-        if (query.trim()) {
+        if (query) {
           const images = await getImagesByText(query, 4);
           this.images = images;
         }
@@ -46,48 +39,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.searchbar {
-  width: 60vw;
-  height: 40px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  margin: 20px auto;
-
-  &__field {
-    border: 1px solid black;
-    width: inherit;
-    height: inherit;
-    border-radius: 10px;
-    padding: 0 40px;
-    caret-color: #9babfe;
-
-    &:focus {
-      outline: 2px solid #9babfe;
-      border: none;
-    }
-  }
-
-
-  .btn {
-    position: absolute;
-  }
-
-  .btn-left {
-    left: 5px;
-  }
-
-  .btn-right {
-    right: 5px;
-    cursor: pointer;
-
-    &:hover {
-      color: #9babfe;
-    }
-  }
-}
-
 .card-container {
   display: flex;
   flex-wrap: wrap;
---
+++ src/components/SearchbarComponent.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="searchbar">
+        <IconSearch class="btn btn-left" :size="30" />
+        <input class="searchbar__field" type="text" v-model="searchTerm" @keypress.enter="sendText">
+        <IconX class="btn btn-right" :size="30" />
+    </div>
+</template>
+
+<script>
+import { IconSearch, IconX } from '@tabler/icons-vue'
+
+export default {
+    components: {
+        IconSearch,
+        IconX
+    },
+    data() {
+        return {
+            searchTerm: '',
+        }
+    },
+    methods: {
+        sendText() {
+            if (this.searchTerm.trim()) {
+                this.$emit('queryText', this.searchTerm);
+                console.log(this.searchTerm)
+                this.searchTerm = '';
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.searchbar {
+    width: 60vw;
+    height: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    margin: 20px auto;
+
+    &__field {
+        border: 1px solid black;
+        width: inherit;
+        height: inherit;
+        border-radius: 10px;
+        padding: 0 40px;
+        caret-color: #9babfe;
+
+        &:focus {
+            outline: 2px solid #9babfe;
+            border: none;
+        }
+    }
+
+
+    .btn {
+        position: absolute;
+    }
+
+    .btn-left {
+        left: 5px;
+    }
+
+    .btn-right {
+        right: 5px;
+        cursor: pointer;
+
+        &:hover {
+            color: #9babfe;
+        }
+    }
+}
+</style>
