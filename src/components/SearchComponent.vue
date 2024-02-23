9bc67272f773cd224c749958d01e2d5875501fa5
cesarisim0 <cedgo1997@gmail.com>
2024-02-23 05:14:44 -0400

    ADD: Native dialog html modal

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -8,6 +8,14 @@
   <div class="image-placeholder-container" v-else>
     <img src="./../assets/search-zoom.png" alt="Zoom image">
     <strong>¡Comienza tu búsqueda y elige las mejores imágenes!</strong>
+
+    <dialog class="final-modal" v-if="dialogOpen">
+      <div class="modal-content">
+        <h2 class="modal-title">¡Felicidades!</h2>
+        <p class="modal-text">Se ha terminado el juego. ¿Deseas empezar de nuevo?</p>
+        <button class="accept-button" @click="startAgain">Aceptar</button>
+      </div>
+    </dialog>
   </div>
 </template>
 
@@ -26,7 +34,8 @@ export default {
   data() {
     return {
       sellersInfo: [],
-      searchText: ''
+      searchText: '',
+      dialogOpen: false
     }
   },
   methods: {
@@ -48,22 +57,31 @@ export default {
       }
     },
     async handleSellerWin(event) {
-      const payload = {
-        status: 'open',
-        numberTemplate: {
-          id: 0
-        },
-        items: [],
-        seller: {
-          id: event.sellerId,
-        },
-        date: new Date(),
-        dueDate: new Date(),
-        client: {
-          id: 7
+      try {
+        const payload = {
+          status: 'open',
+          numberTemplate: {
+            id: 0
+          },
+          items: [],
+          seller: {
+            id: event.sellerId,
+          },
+          date: new Date(),
+          dueDate: new Date(),
+          client: {
+            id: 7
+          }
         }
+        await createInvoice({ ...payload });
+        this.dialogOpen = true;
+      } catch (error) {
+        console.error(error)
       }
-      console.log(await createInvoice({ ...payload }))
+    },
+    startAgain() {
+      this.dialogOpen = false;
+      this.$router.push('/');
     }
   },
 }
@@ -89,5 +107,51 @@ export default {
   align-items: center;
   padding: 30px;
   font-size: 32px;
+}
+
+.final-modal {
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  border-radius: 8px;
+  padding: 20px;
+  text-align: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  /* Sombra ligera */
+}
+
+.modal-title {
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+}
+
+.modal-text {
+  font-size: 1rem;
+  margin-bottom: 20px;
+}
+
+.accept-button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1rem;
+  transition: background-color 0.3s ease;
+}
+
+.accept-button:hover {
+  background-color: #0056b3;
 }
 </style>
