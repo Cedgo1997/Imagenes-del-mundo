34274f4fbc382999744cb394205c202dcc0fb025
cesarisim0 <cedgo1997@gmail.com>
2024-02-22 23:15:46 -0400

    ADD: Import css animation fade id, custom css searchbar

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="card fade-in">
+        <div class="card__top">
+            <div class="user">
+                <img class="user__profile-picture" src="https://placehold.co/40" alt="user-picture">
+                <h3>Cesar Gonzalez</h3>
+            </div>
+        </div>
+
+        <figure class="image-container">
+            <img class="image fade-in" :src="image.src.original" :alt="image.alt">
+        </figure>
+
+        <div class="buttons">
+            <IconHeart class="btn" :size="30" />
+            <IconDownload class="btn" :size="30" />
+        </div>
+    </div>
+</template>
+
+<script>
+import { IconHeart, IconDownload } from '@tabler/icons-vue'
+
+export default {
+    components: {
+        IconHeart,
+        IconDownload
+    },
+    props: {
+        image: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    align-items: center;
+    background: #fff;
+    border-radius: 5px;
+    box-shadow: 15px 15px 60px rgba(0, 0, 0, .15);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    min-height: 400px;
+    padding: 20px;
+    position: relative;
+    width: 310px;
+
+    &__top {
+        align-items: center;
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+
+        .user {
+            width: 100%;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            padding-left: 2px;
+            gap: 10px;
+
+            &__profile-picture {
+                border-radius: 50%;
+            }
+        }
+    }
+}
+
+.buttons {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+
+    .btn {
+        cursor: pointer;
+    }
+}
+
+.image-container {
+    height: 320px;
+    max-height: 300px;
+    overflow: hidden;
+    position: relative;
+    width: 300px;
+    cursor: pointer;
+
+
+    .image {
+        height: 350px;
+        width: 100%;
+    }
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,28 @@
 <template>
   <h1>World Images</h1>
   <!-- Searchbar -->
-  <input type="text" v-model="seearchTerm" @keypress.enter="getImages(seearchTerm)">
+  <div class="searchbar">
+    <IconSearch class="btn btn-left" :size="30" />
+    <input class="searchbar__field" type="text" v-model="seearchTerm" @keypress.enter="getImages(seearchTerm)">
+    <IconX class="btn btn-right" :size="30" />
+  </div>
 
   <!-- Sellers Component -->
-  <div >
-    <div class="card" v-for="image in images" :key="image.id">
-
-      <div class="card__top">
-        <div class="user">
-          <img class="user__profile-picture" src="https://placehold.co/40" alt="user-picture">
-          <h3>Cesar Gonzalez</h3>
-        </div>
-      </div>
-
-
-      <figure class="image-container">
-        <img class="image" :src="image.src.original" :alt="image.alt">
-      </figure>
-
-
-      <div class="buttons">
-        <IconHeart class="btn" :size="30" />
-        <IconDownload class="btn" :size="30" />
-      </div>
-
-    </div>
+  <div class="card-container" v-if="images.length">
+    <CardComponent v-for="image in images" :image="image" :key="image.id" />
   </div>
 </template>
 
 <script>
+import CardComponent from './CardComponent'
 import getImagesByText from '@/helpers/pexels/getPexelsImages';
-import { IconHeart, IconDownload } from '@tabler/icons-vue'
+import { IconSearch, IconX } from '@tabler/icons-vue'
 
 export default {
   components: {
-    IconHeart,
-    IconDownload
+    CardComponent,
+    IconSearch,
+    IconX
   },
   data() {
     return {
@@ -46,10 +32,13 @@ export default {
   },
   methods: {
     async getImages(query) {
-      if (query.trim()) {
-        const images = await getImagesByText(query, 4);
-        this.images = images;
-        console.log(this.images)
+      try {
+        if (query.trim()) {
+          const images = await getImagesByText(query, 4);
+          this.images = images;
+        }
+      } catch (error) {
+        console.error(error);
       }
     }
   },
@@ -57,56 +46,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card {
-  align-items: center;
-  background: #fff;
-  border-radius: 5px;
-  box-shadow: 15px 15px 60px rgba(0, 0, 0, .15);
+.searchbar {
+  width: 60vw;
+  height: 40px;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 400px;
-  padding: 20px;
+  justify-content: space-between;
+  align-items: center;
   position: relative;
-  width: 310px;
+  margin: 20px auto;
 
-  &__top {
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
+  &__field {
+    border: 1px solid black;
+    width: inherit;
+    height: inherit;
+    border-radius: 10px;
+    padding: 0 40px;
+    caret-color: #9babfe;
 
-    .user {
-      width: 100%;
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      padding-left: 2px;
-      gap: 10px;
+    &:focus {
+      outline: 2px solid #9babfe;
+      border: none;
+    }
+  }
 
-      &__profile-picture {
-        border-radius: 50%;
-      }
+
+  .btn {
+    position: absolute;
+  }
+
+  .btn-left {
+    left: 5px;
+  }
+
+  .btn-right {
+    right: 5px;
+    cursor: pointer;
+
+    &:hover {
+      color: #9babfe;
     }
   }
 }
 
-.buttons {
-  .btn {
-    cursor: pointer;
-  }
-}
-
-.image-container {
-  height: 320px;
-  max-height: 300px;
-  overflow: hidden;
-  position: relative;
-  width: 300px;
-
-  .image {
-    height: 350px;
-    width: 100%;
-  }
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
 }
 </style>
